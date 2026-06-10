bf2e5f5f5a204c6b114c6536f29bd70839216ad3
nwh <3055313094@qq.com>
2026-06-10 02:43:20 +0000

    8.4.5

--- example.js
+++ example.js
@@ -3,45 +3,115 @@
 const mqemitter = require('.')
 
 const mq = mqemitter()
+const listeners = new Map()
+const queues = new Map()
 
-function subscribe (topic, obj) {
-  mq.on(topic, callback)
-  obj.close = close
+function subscribe (topic, queue) {
+  if (listeners.has(topic)) {
+    listeners.get(topic).queues.push(queue)
+    queues.get(topic).push(queue)
+    return
+  }
 
-  function callback (value, cb) {
-    obj.push(value)
+  function callback (message, cb) {
+    const queueList = queues.get(topic)
+    if (queueList) {
+      for (const q of queueList) {
+        q.push(message)
+      }
+    }
     cb()
   }
 
-  function close () {
-    mq.removeListener(topic, callback)
+  listeners.set(topic, { callback, queues: [queue] })
+  queues.set(topic, [queue])
+  mq.on(topic, callback)
+
+  return {
+    close: function (done) {
+      const listener = listeners.get(topic)
+      if (listener) {
+        const queueList = queues.get(topic)
+        const idx = queueList ? queueList.indexOf(queue) : -1
+        if (idx !== -1) {
+          queueList.splice(idx, 1)
+        }
+        listener.queues.splice(listener.queues.indexOf(queue), 1)
+
+        if (listener.queues.length === 0) {
+          mq.removeListener(topic, listener.callback, done)
+          listeners.delete(topic)
+          queues.delete(topic)
+        } else if (done) {
+          done()
+        }
+      } else if (done) {
+        done()
+      }
+    }
   }
 }
-
-class MyQueue {
-  push (value) {
-    console.log(value)
-  }
-}
-
-const a = new MyQueue()
-const b = new MyQueue()
-const c = new MyQueue()
-
-subscribe('hello', a)
-subscribe('hello', b)
-subscribe('hello', c)
-
-mq.emit({ topic: 'hello', payload: 'world' })
-
-a.close()
-b.close()
-c.close()
-
-mq.emit({ topic: 'hello', payload: 'world' })
+// const listeners = new Map()
+//
+//
+// const queues = new Map()
+//
+// function subscribe (topic, queue) {
+//   if (listeners.has(topic)) {
+//
+//   }
+//
+//   function callback (err) {
+//
+//     for (var value of queues) {
+//     }
+//   }
+//
+//   listeners.set(topic, callback)
+//   queues.set(topic, [queue])
+// }
 
 // const listeners = new Map()
 //
+//
+// const queues = new Map()
+//
+// function subscribe (topic, queue) {
+//   if (listeners.has(topic)) {
+//
+//   }
+//
+//   function callback (err) {
+//
+//     for (var value of queues) {
+//     }
+//   }
+//
+//   listeners.set(topic, callback)
+//   queues.set(topic, [queue])
+// }
+//
+//   function callback (err) {
+//
+//     for (var value of queues) {
+//     }
+//   }
+//
+//   listeners.set(topic, callback)
+//   queues.set(topic, [queue])
+// }
+//   }
+//
+//   function callback (err) {
+//
+//     for (var value of queues) {
+//     }
+//   }
+//
+//   listeners.set(topic, callback)
+//   queues.set(topic, [queue])
+// }
+
 //
 // const queues = new Map()
 //
